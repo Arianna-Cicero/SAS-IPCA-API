09d9d6ad06cf2ef5de89e605e6a609b0cbfa6552
Arianna Cicero <arianna.cicero.c@gmail.com>
2025-11-11 03:04:18 +0000

    Add: Ktor API project setup

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    application
+}
+
+val ktorVersion = "3.0.0"
+val exposedVersion = "0.50.1"
+val postgresVersion = "42.7.3"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Ktor server
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+
+    // Serialization
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+    // Exposed ORM + PostgreSQL
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("com.ipca.ApplicationKt")
+}
---
+++ src/main/kotlin/com/ipca/Application.kt
@@ -0,0 +1,25 @@
+package com.ipca
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Loja Social IPCA API is running")
+        }
+    }
+}
